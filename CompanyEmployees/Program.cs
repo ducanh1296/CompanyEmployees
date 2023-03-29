1dee48c87084a2bd7f08f6614567fd5f6742f4bc
anhld196 <anhld1@gearinc.com>
2023-03-30 01:40:48 +0700

    2nd

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,8 +1,15 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+    "/nlog.config"));
+builder.Services.ConfigureCors();
 builder.Services.AddControllers();
+builder.Services.ConfigureLoggerService();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,11 +19,23 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
